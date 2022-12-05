67bbff6716634c4867a0e5dec82fe501f0e674ec
jobdecrypt <76563339+jobdecrypt@users.noreply.github.com>
2022-12-04 23:52:12 -0500

    First Commit

---
+++ Course Introduction/intro.py
@@ -0,0 +1,10 @@
+friends = ['Taylor', 'Alex', 'Pat', 'Eli']
+for friend in friends:
+    print('Hi ' + friend)
+
+print('\n')
+for i in range(10):
+    print('Hello, World!')
+
+for i in range(15):
+    print(i)
---
+++ Course Introduction/user.py
@@ -0,0 +1,6 @@
+name = "Brook"
+print("Hello " + name)
+
+#This is a comment
+
+print( 5 % 2 )
